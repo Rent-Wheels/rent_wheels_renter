ab2453c78742bec357dec70e5bb27f026a4829af
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-05-21 18:11:39 +0000

    auth checker added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:rent_wheels_renter/src/home/presentation/home.dart';
+
+import 'package:rent_wheels_renter/tester.dart';
+import 'package:rent_wheels_renter/firebase/auth/auth_service.dart';
+import 'package:rent_wheels_renter/src/login/presentation/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +21,48 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: const ConnectionPage(),
+    );
+  }
+}
+
+class ConnectionPage extends StatefulWidget {
+  const ConnectionPage({super.key});
+
+  @override
+  State<ConnectionPage> createState() => _ConnectionPageState();
+}
+
+class _ConnectionPageState extends State<ConnectionPage> {
+  List appUser = [];
+
+  userStatus() async {
+    await AuthService.firebase().initialize();
+
+    final user = FirebaseAuth.instance.currentUser;
+
+    appUser.add(user);
+
+    return appUser;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: userStatus(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            if (appUser[0] != null) {
+              return const Home();
+            } else {
+              return const Login();
+            }
+
+          default:
+            return const Tester();
+        }
+      },
     );
   }
 }
