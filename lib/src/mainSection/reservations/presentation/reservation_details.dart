ec723a4bf52bde29901a865eb7f496dc111b0377
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-08-03 21:50:35 +0000

    update: reservation details page completed

--- lib/src/mainSection/reservations/presentation/reservation_details.dart
+++ lib/src/mainSection/reservations/presentation/reservation_details.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'package:rent_wheels_renter/src/mainSection/reservations/widgets/reservation_details_widget.dart';
+import 'package:rent_wheels_renter/src/mainSection/reservations/widgets/reservation_details_bottom_sheet_widget.dart';
+
 import 'package:rent_wheels_renter/core/widgets/sizes/sizes.dart';
 import 'package:rent_wheels_renter/core/models/car/car_model.dart';
 import 'package:rent_wheels_renter/core/widgets/theme/colors.dart';
 import 'package:rent_wheels_renter/core/models/user/user_model.dart';
+import 'package:rent_wheels_renter/core/widgets/popups/error_popup.dart';
+import 'package:rent_wheels_renter/core/widgets/popups/success_popup.dart';
 import 'package:rent_wheels_renter/core/models/reservation/reservation_model.dart';
+import 'package:rent_wheels_renter/core/widgets/loadingIndicator/loading_indicator.dart';
 import 'package:rent_wheels_renter/core/widgets/buttons/adaptive_back_button_widget.dart';
-import 'package:rent_wheels_renter/src/mainSection/reservations/widgets/reservation_details_widget.dart';
+import 'package:rent_wheels_renter/core/backend/reservations/methods/reservations_methods.dart';
 
 class ReservationDetails extends StatefulWidget {
   final Car car;
@@ -67,29 +73,59 @@ class _ReservationDetailsState extends State<ReservationDetails> {
           reservation: reservation,
         ),
       ),
-      // bottomSheet: buildReservationDetailsBottomSheet(
-      //         buttonTitle: 'Make Reservation',
-      //         context: context,
-      //         onPressed: () async {
-      //           try {
-      //             buildLoadingIndicator(context, 'Making Reservation');
-      //             await RentWheelsReservationsMethods()
-      //                 .makeReservation(reservationDetails: reservation);
-      //             if (!mounted) return;
-      //             Navigator.pop(context);
-      //             Navigator.push(
-      //               context,
-      //               CupertinoPageRoute(
-      //                 builder: (context) => const ReservationSuccessful(),
-      //               ),
-      //             );
-      //           } catch (e) {
-      //             if (!mounted) return;
-      //             Navigator.pop(context);
-      //             showErrorPopUp(e.toString(), context);
-      //           }
-      //         },
-      //       ),
+      bottomSheet: reservation.status == 'Pending'
+          ? buildReservationDetailsBottomSheet(
+              context: context,
+              onAccept: () async {
+                try {
+                  buildLoadingIndicator(context, '');
+                  await RentWheelsReservationsMethods().updateReservationStatus(
+                    reservationId: reservation.id!,
+                    status: 'Accepted',
+                  );
+
+                  setState(() {
+                    reservation.status = 'Accepted';
+                  });
+
+                  if (!mounted) return;
+                  Navigator.pop(context);
+                  showSuccessPopUp(
+                    'Reservation Accepted!',
+                    context,
+                  );
+                } catch (e) {
+                  if (!mounted) return;
+                  Navigator.pop(context);
+                  showErrorPopUp(e.toString(), context);
+                }
+              },
+              onDecline: () async {
+                try {
+                  buildLoadingIndicator(context, '');
+                  await RentWheelsReservationsMethods().updateReservationStatus(
+                    reservationId: reservation.id!,
+                    status: 'Cancelled',
+                  );
+
+                  setState(() {
+                    reservation.status = 'Cancelled';
+                  });
+
+                  if (!mounted) return;
+                  Navigator.pop(context);
+                  showSuccessPopUp(
+                    'Reservation Declined!',
+                    context,
+                  );
+                } catch (e) {
+                  if (!mounted) return;
+                  Navigator.pop(context);
+                  showErrorPopUp(e.toString(), context);
+                }
+              },
+            )
+          : null,
     );
   }
 }
